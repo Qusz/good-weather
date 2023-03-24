6bb55f1aa37a7478574539421e38f23d0f621cb5
Qusz <de.gorchakov@gmail.com>
2023-03-24 13:04:43 +0300

    imporves typing

--- src/js/modules/render-location.ts
+++ src/js/modules/render-location.ts
@@ -1,9 +1,11 @@
+import { isHTMLParagraphElement } from './htmlTypePredicates';
+
 export default function (city: string, country: string): void {
   const element: HTMLParagraphElement | null = document.querySelector(
     '.weather-card__location'
   );
 
-  if (element) {
+  if (isHTMLParagraphElement(element)) {
     element.textContent = `${city}, ${country}`;
   }
 }
