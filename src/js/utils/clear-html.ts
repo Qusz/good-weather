14bfcf9003b947810d43428d72ebc720e0922a67
Qusz <de.gorchakov@gmail.com>
2023-03-24 13:15:31 +0300

    improves typing

--- src/js/utils/clear-html.ts
+++ src/js/utils/clear-html.ts
@@ -1,6 +1,10 @@
+import { isHTMLElement } from '../modules/htmlTypePredicates';
+
 export default function (element: HTMLElement | null): void {
-  if (element?.innerHTML) {
-    // eslint-disable-next-line no-param-reassign
-    element.innerHTML = '';
+  if (isHTMLElement(element)) {
+    if (element.innerHTML) {
+      // eslint-disable-next-line no-param-reassign
+      element.innerHTML = '';
+    }
   }
 }
