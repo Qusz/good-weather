1b438540e7bfafd553ce4d283b57141f67d54a73
Qusz <de.gorchakov@gmail.com>
2023-04-08 12:10:11 +0300

    removes redundant DOMContentLoaded event

--- src/js/modules/load-content.ts
+++ src/js/modules/load-content.ts
@@ -12,62 +12,60 @@ import renderCurrentDatetime from './render-current-datetime';
 import { isHTMLElement } from './html-type-predicates';
 
 export default function (): void {
-  document.addEventListener('DOMContentLoaded', () => {
-    const localStorageExists = checkLocalStorage();
-    const tableBody: HTMLElement | null = document.querySelector(
-      '[data-forecast-table-body]'
-    );
+  const localStorageExists = checkLocalStorage();
+  const tableBody: HTMLElement | null = document.querySelector(
+    '[data-forecast-table-body]'
+  );
 
-    if (isHTMLElement(tableBody)) {
-      if (localStorageExists) {
-        const location = getSavedLocation();
+  if (isHTMLElement(tableBody)) {
+    if (localStorageExists) {
+      const location = getSavedLocation();
 
-        renderLocation(location.city, location.country);
+      renderLocation(location.city, location.country);
 
-        openmeteo(location.lat, location.lon)
-          .then((data) => {
-            renderCurrentWeather(data);
-            return data;
-          })
-          .then((data) => {
-            renderForecast(tableBody, data);
-            return data;
-          })
-          .then((data) => {
-            return worldtime(data.timezone);
-          })
-          .then((data) => {
-            renderCurrentDatetime(data.datetime, data.timezone);
-          })
-          .catch((error) => {
-            showAlert(error.message);
-          });
-      } else {
-        getLocation()
-          .then((location) => {
-            renderLocation(location.city, location.country);
-            return openmeteo(location.latitude, location.longitude);
-          })
-          .then((weatherData) => {
-            renderCurrentWeather(weatherData);
-            return weatherData;
-          })
-          .then((weatherData) => {
-            renderForecast(tableBody, weatherData);
-            return weatherData;
-          })
-          .then((weatherData) => {
-            return worldtime(weatherData.timezone);
-          })
-          .then((timeData) => {
-            renderCurrentDatetime(timeData.datetime, timeData.timezone);
-          })
-          .catch((error) => {
-            showAlert(error.message);
-          });
-      }
+      openmeteo(location.lat, location.lon)
+        .then((data) => {
+          renderCurrentWeather(data);
+          return data;
+        })
+        .then((data) => {
+          renderForecast(tableBody, data);
+          return data;
+        })
+        .then((data) => {
+          return worldtime(data.timezone);
+        })
+        .then((data) => {
+          renderCurrentDatetime(data.datetime, data.timezone);
+        })
+        .catch((error) => {
+          showAlert(error.message);
+        });
     } else {
-      showAlert('Unexpected Error: Some DOM elements are missing.');
+      getLocation()
+        .then((location) => {
+          renderLocation(location.city, location.country);
+          return openmeteo(location.latitude, location.longitude);
+        })
+        .then((weatherData) => {
+          renderCurrentWeather(weatherData);
+          return weatherData;
+        })
+        .then((weatherData) => {
+          renderForecast(tableBody, weatherData);
+          return weatherData;
+        })
+        .then((weatherData) => {
+          return worldtime(weatherData.timezone);
+        })
+        .then((timeData) => {
+          renderCurrentDatetime(timeData.datetime, timeData.timezone);
+        })
+        .catch((error) => {
+          showAlert(error.message);
+        });
     }
-  });
+  } else {
+    showAlert('Unexpected Error: Some DOM elements are missing.');
+  }
 }
