c0450278bd26653487858356e82a5835ed78294f
Qusz <de.gorchakov@gmail.com>
2023-04-08 14:51:24 +0300

    removes redundant typing

--- src/js/APIs/worldtime.ts
+++ src/js/APIs/worldtime.ts
@@ -4,7 +4,7 @@ export default async function (timezone: string): Promise<WorldtimeResponse> {
   const url = `https://worldtimeapi.org/api/timezone/${timezone}`;
 
   try {
-    const request: Response = await fetch(url);
+    const request = await fetch(url);
     const response: WorldtimeResponse = await request.json();
 
     return response;
