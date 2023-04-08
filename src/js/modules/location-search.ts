6706eeb967845cd8f7002c9ccab246356b4328f4
Qusz <de.gorchakov@gmail.com>
2023-04-08 14:16:53 +0300

    removes nested if ... else

--- src/js/modules/location-search.ts
+++ src/js/modules/location-search.ts
@@ -6,6 +6,7 @@ import clearHTML from 'utils/clear-html';
 import showAlert from 'utils/show-alert';
 import { saveLocation } from 'utils/local-storage';
 import { isGuaranteedLocationElements } from 'utils/guaranteed-elements';
+
 import openmeteo from '../APIs/openmeteo';
 import worldtime from '../APIs/worldtime';
 import nominatim from '../APIs/nominatim';
@@ -20,111 +21,112 @@ import { getLocationElements } from './elements-selector';
 export default function (): void {
   const locationElements = getLocationElements();
 
-  if (isGuaranteedLocationElements(locationElements)) {
-    // Using lodash debounce to avoid spamming the API with requests
-    const handleSearch = debounce(() => {
-      const query = locationElements.searchInput.value;
+  if (!isGuaranteedLocationElements(locationElements)) {
+    showAlert('Unexpected Error: Some DOM elements are missing.');
+    return;
+  }
 
-      if (query) {
-        nominatim(query)
-          .then((data) => {
-            clearHTML(locationElements.searchResultsList);
-            locationElements.searchResultsParent.style.display = 'block';
+  // Using lodash debounce to avoid spamming the API with requests
+  const handleSearch = debounce(() => {
+    const query = locationElements.searchInput.value;
 
-            data.forEach((item) => {
-              /*
+    if (query) {
+      nominatim(query)
+        .then((data) => {
+          clearHTML(locationElements.searchResultsList);
+          locationElements.searchResultsParent.style.display = 'block';
+
+          data.forEach((item) => {
+            /*
               The location name property in item.address object can have different names depending on location's type. So it's accessed via index rather than property name
             */
-              const itemKeys = Object.keys(item.address);
+            const itemKeys = Object.keys(item.address);
 
-              const li = document.createElement('li') as HTMLLIElement;
-              li.className = 'searchbar__results-item';
-              li.innerHTML = `${item.display_name}`;
-              li.setAttribute('data-place-name', item.address[itemKeys[0]]);
-              li.setAttribute('data-place-country', item.address.country);
-              li.setAttribute('data-place-lon', item.lon);
-              li.setAttribute('data-place-lat', item.lat);
+            const li = document.createElement('li') as HTMLLIElement;
+            li.className = 'searchbar__results-item';
+            li.innerHTML = `${item.display_name}`;
+            li.setAttribute('data-place-name', item.address[itemKeys[0]]);
+            li.setAttribute('data-place-country', item.address.country);
+            li.setAttribute('data-place-lon', item.lon);
+            li.setAttribute('data-place-lat', item.lat);
 
-              locationElements.searchResultsList.appendChild(li);
-            });
-
-            locationElements.searchResultsParent.style.display = 'block';
-          })
-          .catch((error) => {
-            clearHTML(locationElements.searchResultsList);
-
-            const message = document.createElement('li') as HTMLLIElement;
-            message.className = 'searchbar__results-message';
-            message.textContent = error.message;
-            locationElements.searchResultsList.appendChild(message);
+            locationElements.searchResultsList.appendChild(li);
           });
 
-        // Abort search if query is empty
-      } else if (!query) {
-        locationElements.searchResultsParent.style.display = 'none';
-      }
-    }, 300);
-
-    /*
-     * Events
-     */
-    locationElements.searchInput.addEventListener('keyup', handleSearch);
-
-    // Choose location
-    locationElements.searchResultsList.addEventListener('click', (e: Event) => {
-      const target = e.target as HTMLElement;
-      if (!target.classList.contains('searchbar__results-item')) return;
-
-      // Close search menu when location is chosen
-      clearHTML(locationElements.searchResultsList);
-      locationElements.searchResultsParent.style.display = 'none';
-
-      const location: LocalStorageData = {
-        city: target.getAttribute('data-place-name')!,
-        country: target.getAttribute('data-place-country')!,
-        lon: target.getAttribute('data-place-lon')!,
-        lat: target.getAttribute('data-place-lat')!
-      };
-
-      saveLocation(location);
-
-      renderLocation(location.city, location.country);
-
-      openmeteo(location.lat, location.lon)
-        .then((data) => {
-          renderCurrentWeather(data);
-          return data;
-        })
-        .then((data) => {
-          renderForecast(locationElements.tableBody, data);
-          return data;
-        })
-        .then((data) => {
-          return worldtime(data.timezone);
-        })
-        .then((data) => {
-          renderCurrentDatetime(data.datetime, data.timezone);
+          locationElements.searchResultsParent.style.display = 'block';
         })
         .catch((error) => {
-          showAlert(error.message);
+          clearHTML(locationElements.searchResultsList);
+
+          const message = document.createElement('li') as HTMLLIElement;
+          message.className = 'searchbar__results-message';
+          message.textContent = error.message;
+          locationElements.searchResultsList.appendChild(message);
         });
-    });
 
-    // Close search result when clicked anywhere
-    document.addEventListener('click', (e: Event) => {
-      if (
-        !locationElements.searchForm.contains(e.target as HTMLElement) &&
-        locationElements.searchResultsParent.style.display !== 'none'
-      ) {
-        clearHTML(locationElements.searchResultsList);
-        locationElements.searchResultsParent.style.display = 'none';
-      }
-    });
+      // Abort search if query is empty
+    } else if (!query) {
+      locationElements.searchResultsParent.style.display = 'none';
+    }
+  }, 300);
 
-    locationElements.searchForm.addEventListener('submit', (e) => {
-      e.preventDefault();
-    });
-  } else {
-    showAlert('Unexpected Error: Some DOM elements are missing.');
-  }
+  /*
+   * Events
+   */
+  locationElements.searchInput.addEventListener('keyup', handleSearch);
+
+  // Choose location
+  locationElements.searchResultsList.addEventListener('click', (e: Event) => {
+    const target = e.target as HTMLElement;
+    if (!target.classList.contains('searchbar__results-item')) return;
+
+    // Close search menu when location is chosen
+    clearHTML(locationElements.searchResultsList);
+    locationElements.searchResultsParent.style.display = 'none';
+
+    const location: LocalStorageData = {
+      city: target.getAttribute('data-place-name')!,
+      country: target.getAttribute('data-place-country')!,
+      lon: target.getAttribute('data-place-lon')!,
+      lat: target.getAttribute('data-place-lat')!
+    };
+
+    saveLocation(location);
+
+    renderLocation(location.city, location.country);
+
+    openmeteo(location.lat, location.lon)
+      .then((data) => {
+        renderCurrentWeather(data);
+        return data;
+      })
+      .then((data) => {
+        renderForecast(locationElements.tableBody, data);
+        return data;
+      })
+      .then((data) => {
+        return worldtime(data.timezone);
+      })
+      .then((data) => {
+        renderCurrentDatetime(data.datetime, data.timezone);
+      })
+      .catch((error) => {
+        showAlert(error.message);
+      });
+  });
+
+  // Close search result when clicked anywhere
+  document.addEventListener('click', (e: Event) => {
+    if (
+      !locationElements.searchForm.contains(e.target as HTMLElement) &&
+      locationElements.searchResultsParent.style.display !== 'none'
+    ) {
+      clearHTML(locationElements.searchResultsList);
+      locationElements.searchResultsParent.style.display = 'none';
+    }
+  });
+
+  locationElements.searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+  });
 }
