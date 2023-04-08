d1260537a583f208842c42374c77da7a89b7ad24
Qusz <de.gorchakov@gmail.com>
2023-04-08 13:37:25 +0300

    extracts element selector and type guard logic

--- src/js/modules/location-search.ts
+++ src/js/modules/location-search.ts
@@ -1,10 +1,11 @@
 import debounce from 'lodash.debounce';
 
-import { LocalStorageData, GuaranteedElements } from 'types/types';
+import { LocalStorageData } from 'types/types';
 
 import clearHTML from 'utils/clear-html';
 import showAlert from 'utils/show-alert';
 import { saveLocation } from 'utils/local-storage';
+import { isGuaranteedLocationElements } from 'utils/guaranteed-elements';
 import openmeteo from '../APIs/openmeteo';
 import worldtime from '../APIs/worldtime';
 import nominatim from '../APIs/nominatim';
@@ -14,78 +15,27 @@ import renderForecast from './render-forecast';
 import renderLocation from './render-location';
 import renderCurrentDatetime from './render-current-datetime';
 
-import {
-  isHTMLDivElement,
-  isHTMLElement,
-  isHTMLFormElement,
-  isHTMLInputElement,
-  isHTMLUListElement
-} from './html-type-predicates';
-
-type Elements = {
-  searchForm: HTMLFormElement | null;
-  searchResultsParent: HTMLDivElement | null;
-  searchInput: HTMLInputElement | null;
-  searchResultsList: HTMLUListElement | null;
-  tableBody: HTMLElement | null;
-};
-
-function isGuaranteedElements(
-  elements: Elements | GuaranteedElements<Elements>
-): elements is GuaranteedElements<Elements> {
-  return (
-    isHTMLFormElement(elements.searchForm) &&
-    isHTMLDivElement(elements.searchResultsParent) &&
-    isHTMLInputElement(elements.searchInput) &&
-    isHTMLUListElement(elements.searchResultsList) &&
-    isHTMLElement(elements.tableBody)
-  );
-}
+import { getLocationElements } from './elements-selector';
 
 export default function (): void {
-  const searchForm: HTMLFormElement | null =
-    document.querySelector('[data-search]');
+  const locationElements = getLocationElements();
 
-  const searchResultsParent: HTMLDivElement | null = document.querySelector(
-    '[data-search-results]'
-  );
-
-  const searchInput: HTMLInputElement | null = document.querySelector(
-    '[data-search-input]'
-  );
-
-  const searchResultsList: HTMLUListElement | null = document.querySelector(
-    '[data-search-results-list]'
-  );
-
-  const tableBody: HTMLElement | null = document.querySelector(
-    '[data-forecast-table-body]'
-  );
-
-  const elements: Elements = {
-    searchForm,
-    searchResultsParent,
-    searchInput,
-    searchResultsList,
-    tableBody
-  };
-
-  if (isGuaranteedElements(elements)) {
+  if (isGuaranteedLocationElements(locationElements)) {
     // Using lodash debounce to avoid spamming the API with requests
     const handleSearch = debounce(() => {
-      const query: string | undefined = elements.searchInput.value;
+      const query = locationElements.searchInput.value;
 
       if (query) {
         nominatim(query)
           .then((data) => {
-            clearHTML(elements.searchResultsList);
-            elements.searchResultsParent.style.display = 'block';
+            clearHTML(locationElements.searchResultsList);
+            locationElements.searchResultsParent.style.display = 'block';
 
             data.forEach((item) => {
               /*
-                The location name property in item.address object can have different names depending on location's type. So it's accessed via index rather than property name
-              */
-              const itemKeys: string[] = Object.keys(item.address);
+              The location name property in item.address object can have different names depending on location's type. So it's accessed via index rather than property name
+            */
+              const itemKeys = Object.keys(item.address);
 
               const li = document.createElement('li') as HTMLLIElement;
               li.className = 'searchbar__results-item';
@@ -95,39 +45,39 @@ export default function (): void {
               li.setAttribute('data-place-lon', item.lon);
               li.setAttribute('data-place-lat', item.lat);
 
-              elements.searchResultsList.appendChild(li);
+              locationElements.searchResultsList.appendChild(li);
             });
 
-            elements.searchResultsParent.style.display = 'block';
+            locationElements.searchResultsParent.style.display = 'block';
           })
           .catch((error) => {
-            clearHTML(elements.searchResultsList);
+            clearHTML(locationElements.searchResultsList);
 
             const message = document.createElement('li') as HTMLLIElement;
             message.className = 'searchbar__results-message';
             message.textContent = error.message;
-            elements.searchResultsList.appendChild(message);
+            locationElements.searchResultsList.appendChild(message);
           });
 
         // Abort search if query is empty
       } else if (!query) {
-        elements.searchResultsParent.style.display = 'none';
+        locationElements.searchResultsParent.style.display = 'none';
       }
     }, 300);
 
     /*
      * Events
      */
-    elements.searchInput.addEventListener('keyup', handleSearch);
+    locationElements.searchInput.addEventListener('keyup', handleSearch);
 
     // Choose location
-    elements.searchResultsList.addEventListener('click', (e: Event) => {
+    locationElements.searchResultsList.addEventListener('click', (e: Event) => {
       const target = e.target as HTMLElement;
       if (!target.classList.contains('searchbar__results-item')) return;
 
       // Close search menu when location is chosen
-      clearHTML(elements.searchResultsList);
-      elements.searchResultsParent.style.display = 'none';
+      clearHTML(locationElements.searchResultsList);
+      locationElements.searchResultsParent.style.display = 'none';
 
       const location: LocalStorageData = {
         city: target.getAttribute('data-place-name')!,
@@ -146,7 +96,7 @@ export default function (): void {
           return data;
         })
         .then((data) => {
-          renderForecast(elements.tableBody, data);
+          renderForecast(locationElements.tableBody, data);
           return data;
         })
         .then((data) => {
@@ -163,15 +113,15 @@ export default function (): void {
     // Close search result when clicked anywhere
     document.addEventListener('click', (e: Event) => {
       if (
-        !elements.searchForm.contains(e.target as HTMLElement) &&
-        elements.searchResultsParent.style.display !== 'none'
+        !locationElements.searchForm.contains(e.target as HTMLElement) &&
+        locationElements.searchResultsParent.style.display !== 'none'
       ) {
-        clearHTML(elements.searchResultsList);
-        elements.searchResultsParent.style.display = 'none';
+        clearHTML(locationElements.searchResultsList);
+        locationElements.searchResultsParent.style.display = 'none';
       }
     });
 
-    elements.searchForm.addEventListener('submit', (e) => {
+    locationElements.searchForm.addEventListener('submit', (e) => {
       e.preventDefault();
     });
   } else {
