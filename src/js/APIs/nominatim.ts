6e24cc48d3edde34550c420b10220b6b17eefd3d
Qusz <de.gorchakov@gmail.com>
2023-04-08 14:51:15 +0300

    removes redundant typing

--- src/js/APIs/nominatim.ts
+++ src/js/APIs/nominatim.ts
@@ -1,14 +1,14 @@
 import { NominatimResponse } from 'types/types';
 
 export default async function (query: string): Promise<NominatimResponse[]> {
-  const url: URL = new URL('https://nominatim.openstreetmap.org/search');
+  const url = new URL('https://nominatim.openstreetmap.org/search');
   url.searchParams.set('city', query);
   url.searchParams.set('format', 'json');
   url.searchParams.set('addressdetails', '1');
   url.searchParams.set('accept-language', 'en-US');
 
   try {
-    const request: Response = await fetch(url);
+    const request = await fetch(url);
     const response: NominatimResponse[] = await request.json();
 
     if (!response.length) {
