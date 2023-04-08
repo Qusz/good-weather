dc7bbc5618f1a6d198e3aaa97836ea79a0c5da94
Qusz <de.gorchakov@gmail.com>
2023-04-08 14:44:21 +0300

    extracts elements selection logic into a separate module

---
+++ src/js/modules/elements-selector.ts
@@ -0,0 +1,67 @@
+import {
+  WeatherElements,
+  LocationElements,
+  DateTimeElements,
+  RenderLocationElements,
+  ToggleForecastElemnets,
+  TableElements
+} from 'types/types';
+
+function getCurrentWeatherElements(): WeatherElements {
+  return {
+    currentTemperature: document.querySelector('[data-current-temperature]'),
+    currentWeather: document.querySelector('[data-current-temperature]'),
+    realFeel: document.querySelector('[data-real-feel]'),
+    wind: document.querySelector('[data-wind]'),
+    pressure: document.querySelector('[data-pressure]'),
+    humidity: document.querySelector('[data-humidity]'),
+    sunrise: document.querySelector('[data-sunrise]'),
+    sunset: document.querySelector('[data-sunset]'),
+    icon: document.querySelector('[data-weather-icon-sprite]')
+  };
+}
+
+function getLocationElements(): LocationElements {
+  return {
+    searchForm: document.querySelector('[data-search]'),
+    searchResultsParent: document.querySelector('[data-search-results]'),
+    searchInput: document.querySelector('[data-search-input]'),
+    searchResultsList: document.querySelector('[data-search-results-list]')
+  };
+}
+
+function getCurrentDatetimeElements(): DateTimeElements {
+  return {
+    dateTimeBody: document.querySelector('.weather-card__date')
+  };
+}
+
+function getRenderLocationElements(): RenderLocationElements {
+  return {
+    locationBody: document.querySelector('.weather-card__location')
+  };
+}
+
+function getToggleForecastElements(): ToggleForecastElemnets {
+  return {
+    parent: document.querySelector('[data-forecast]'),
+    toggle: document.querySelector('[data-forecast-header-wrapper]'),
+    caption: document.querySelector('[data-forecast-caption]'),
+    body: document.querySelector('[data-forecast-body]')
+  };
+}
+
+function getTableElements(): TableElements {
+  return {
+    body: document.querySelector('[data-forecast-table-body]')
+  };
+}
+
+export {
+  getCurrentWeatherElements,
+  getLocationElements,
+  getCurrentDatetimeElements,
+  getRenderLocationElements,
+  getToggleForecastElements,
+  getTableElements
+};
